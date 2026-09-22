e418f283547cfb6fd86bc473cd771f7951bdc3ae
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-09-23 00:04:08 +0530

    solved isEvenOdd

---
+++ isEvenOrOddNum.js
@@ -0,0 +1,7 @@
+// create a function to check if a number is even or odd.
+function isEven(num) {
+    if(num % 2 == 0) return "Even"
+    else return "Odd"
+}
+let res = isEven(-5);
+console.log(res);
