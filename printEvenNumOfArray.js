d40a9e44e51d2626bda6c0384fd5738799bf9b6a
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-09-24 22:25:52 +0530

    solved printEvenNumber in array

---
+++ printEvenNumOfArray.js
@@ -0,0 +1,7 @@
+// print all the even numbers in the array.
+let arr = [10, 5, 7, 0, 9, 2, 80];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i]);
+    }
+}
